c23e027946ae87ff981b8b3c35ca651316e422ec
Holger Benl <hbenl@evandor.de>
2017-10-31 13:29:22 +0100

    show the fake test tree in the explorer

--- src/explorer.ts
+++ src/explorer.ts
@@ -1,18 +1,54 @@
 import * as vscode from 'vscode';
+import { TestRunnerAdapter, TestItem } from './adapter/api';
 
-export class TestExplorer implements vscode.TreeDataProvider<vscode.TreeItem> {
+export class TestExplorer implements vscode.TreeDataProvider<TestExplorerItem> {
 
-	onDidChangeTreeData?: vscode.Event<vscode.TreeItem>;
+	private tree?: TestExplorerItem;
+	private readonly treeDataChanged = new vscode.EventEmitter<TestExplorerItem>();
+	public readonly onDidChangeTreeData: vscode.Event<TestExplorerItem>;
 
-	getTreeItem(element: vscode.TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
-		return element;
+	constructor(
+		private readonly adapter: TestRunnerAdapter
+	) {
+		this.onDidChangeTreeData = this.treeDataChanged.event;
+
+		this.adapter.tests.subscribe((suite) => {
+			this.tree = transform(suite);
+			this.treeDataChanged.fire();
+		});
+
+		this.adapter.reloadTests();
 	}
 
-	getChildren(element?: vscode.TreeItem): vscode.ProviderResult<vscode.TreeItem[]> {
-		return [
-			{
-				label: 'Test Explorer Root'
-			}
-		];
+	getTreeItem(item: TestExplorerItem): vscode.TreeItem {
+		return item;
+	}
+
+	getChildren(item?: TestExplorerItem): vscode.ProviderResult<TestExplorerItem[]> {
+		const parent = item || this.tree;
+		return parent ? parent.children : [];
+	}
+}
+
+function transform(item: TestItem): TestExplorerItem {
+
+	if (item.type === 'suite') {
+
+		var children = item.children.map(transform);
+		return new TestExplorerItem(item, children);
+
+	} else {
+
+		return new TestExplorerItem(item, []);
+		
+	}
+}
+
+class TestExplorerItem extends vscode.TreeItem {
+	constructor(
+		public readonly testItem: TestItem,
+		public readonly children: TestExplorerItem[]
+	) {
+		super(testItem.label);
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import * as vscode from 'vscode';
 import { TestExplorer } from './explorer';
+import { FakeAdapter } from './adapter/fake';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const testExplorer = new TestExplorer();
+	const testExplorer = new TestExplorer(new FakeAdapter());
 
 	context.subscriptions.push(vscode.window.registerTreeDataProvider(
 		'extension.test-explorer.tests', testExplorer
